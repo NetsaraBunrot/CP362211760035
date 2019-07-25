daf4a2c88d27ac9825e3900c7494ae4d1e8ae584
netsara <netsaea.b1503@gmail.com>
2019-07-25 16:58:38 +0700

    netsara

---
+++ src/lab3/HelloWorld.java
@@ -0,0 +1,14 @@
+package lab3;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        int x=100;
+        System.out.println("Hello,World!!!\n");
+        System.out.println("\tNetsara Bunrot");
+        System.out.println("362211760035");
+        System.out.println("netsara.b1503@gmail.com");
+        System.out.println("การจัดการเทคโนโลยีสารสนเทศ"+x);
+
+
+    }}//class
---
+++ src/lab4/Exinput.java
@@ -0,0 +1,51 @@
+package lab4;
+
+import java.util.Scanner;
+
+public class Exinput {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please,emter an integer: ");
+        int x = scanner.nextInt();
+
+        System.out.println("Your entered integer: "+x);
+
+        System.out.print("Please,emter an into: ");
+        double y = scanner.nextDouble();
+
+        System.out.println("Your entered integer: "+y);
+
+        //input double
+        System.out.print("please, enter double:");
+        double y = scanner,nextDouble();
+
+        System.out.print("your entered double:"+y);
+
+
+        //input String
+        System.out.print("please' enter your:");
+        String s =scanner.findInLine();
+
+        System.out.println("you name is:"+s);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
