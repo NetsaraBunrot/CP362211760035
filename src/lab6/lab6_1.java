ac3cb0c82e0180f3c33221007242ed4332f98c7a
netsara <netsaea.b1503@gmail.com>
2019-08-22 16:35:20 +0700

    lab6

--- src/lab6/lab6_1.java
+++ src/lab6/lab6_1.java
@@ -4,15 +4,13 @@ import java.util.Scanner;
 
 public class lab6_1 {
     public static void main (String[] args) {
-
-
-       Scanner kb = new Scanner(System.in);
-       double c;
-       System.out.println("Input a drhree in celsius");
-       c = Sc.nextDouble();
-       double f = (9*c+(32*5))/5;
-       System.out.println(c+ "degree Celsius is equal to "+f+" in fahrenh");
-       }
+        Scanner kb = new Scanner(System.in);
+        double c;
+        System.out.println("Input a drhree in celsius");
+        c = Scanner.nextDouble();
+        double f = (9*c+(32*5))/5;
+        System.out.println(c+ "degree Celsius is equal to "+f+" in fahrenh");
+    }
 
 
 
